da46c3e05cd2deb8926590e216163adad982146a
FlorinCS <81389602+FlorinCS@users.noreply.github.com>
2022-02-04 20:34:19 +0100

    Add files via upload

---
+++ Filtru/index.js
@@ -0,0 +1,469 @@
+apple=document.getElementById("apple")  ;           option1=document.getElementById("option1")
+google=document.getElementById("google");            option2=document.getElementById("option2")
+samsung=document.getElementById("samsung");         option3=document.getElementById("option3")
+xiaomi=document.getElementById("xiaomi");              option4=document.getElementById("option4")
+one=document.getElementById("oneplus");             option5=document.getElementById("option5")
+huawei=document.getElementById("huawei");             option6=document.getElementById("option6")
+microsoft=document.getElementById("microsoft");       option7=document.getElementById("option6")
+motorola=document.getElementById("motorola");          option8=document.getElementById("option7")
+lg=document.getElementById("lg");                   option9=document.getElementById("option8")
+nokia=document.getElementById("nokia");            option10=document.getElementById("option9")
+oppo=document.getElementById("oppo");               option11=document.getElementById("option10")
+sony=document.getElementById("sony");              option11=document.getElementById("option11")
+auswahl=document.getElementById("auswahl");           option12=document.getElementById("option12")
+
+
+option13=document.getElementById("option13");      
+option14=document.getElementById("option14");
+option15=document.getElementById("option15");
+option16=document.getElementById("option16");
+option17=document.getElementById("option17");
+option18=document.getElementById("option18");
+option19=document.getElementById("option19");
+option20=document.getElementById("option20");
+option21=document.getElementById("option21");
+option22=document.getElementById("option22");
+option23=document.getElementById("option23");
+option24=document.getElementById("option24");
+
+
+option25=document.getElementById("option25")
+option26=document.getElementById("option26")
+option27=document.getElementById("option27")
+option28=document.getElementById("option28")
+option29=document.getElementById("option29")
+option30=document.getElementById("option30")
+option31=document.getElementById("option31")
+option32=document.getElementById("option32")
+option33=document.getElementById("option33")
+option34=document.getElementById("option34")
+option35=document.getElementById("option35")
+option36=document.getElementById("option36")
+
+
+option37=document.getElementById("option37")
+option38=document.getElementById("option38")
+option39=document.getElementById("option39")
+option40=document.getElementById("option40")
+option41=document.getElementById("option41")
+option42=document.getElementById("option42")
+option43=document.getElementById("option43")
+option44=document.getElementById("option44")
+option45=document.getElementById("option45")
+option46=document.getElementById("option46")
+option47=document.getElementById("option47")
+option48=document.getElementById("option48")
+
+option49=document.getElementById("option49")
+option50=document.getElementById("option50")
+option51=document.getElementById("option51")
+option52=document.getElementById("option52")
+option53=document.getElementById("option53")
+option54=document.getElementById("option54")
+option55=document.getElementById("option55")
+option56=document.getElementById("option56")
+option57=document.getElementById("option57")
+option58=document.getElementById("option58")
+
+optiona=document.getElementById("optiona")
+optionb=document.getElementById("optionb")
+optionc=document.getElementById("optionc")
+optiond=document.getElementById("optiond")
+optione=document.getElementById("optione")
+optionf=document.getElementById("optionf")
+optiong=document.getElementById("optiong")
+optionh=document.getElementById("optionh")
+optioni=document.getElementById("optioni")
+optionj=document.getElementById("optionj")
+optionk=document.getElementById("optionk")
+optionl=document.getElementById("optionl")
+optionm=document.getElementById("optionm")
+optionn=document.getElementById("optionn")
+optiono=document.getElementById("optiono")
+optionp=document.getElementById("optionp")
+
+
+
+
+links=document.getElementById("links")
+
+
+
+function testul(){
+
+    let select=document.getElementById("hersteller")
+     let current=select.options[select.selectedIndex].text;
+  if (current=="Apple"){
+
+     auswahl.innerHTML="Apple Geräte auswählen";
+    option1.style.display="unset"
+    option1.innerHTML="Apple Watch"
+    option2.style.display="unset"
+    option2.innerHTML="Apple Watch SE"
+    option3.style.display="unset"
+    option3.innerHTML="Apple Watch Series 1"
+    option4.style.display="unset"
+    option4.innerHTML="Apple Watch Series 2"
+    option5.style.display="unset"
+    option5.innerHTML="Apple Watch Series 3"
+    option6.style.display="unset"
+    option6.innerHTML="Apple Watch Series 4"
+    option7.style.display="unset"
+    option7.innerHTML="Apple Watch Series 5"
+    option8.style.display="unset"
+    option8.innerHTML="Apple Watch Series 6"
+    option9.style.display="unset"
+    option9.innerHTML="Apple Watch Series 7"
+    option10.style.display="unset"
+    option10.innerHTML="Geschenkgutschein"
+    option11.style.display="unset"
+    option11.innerHTML="iPads"
+    option12.style.display="unset"
+    option12.innerHTML="iPad 2. Generation(2011)"
+    option13.style.display="unset"
+    option13.innerHTML="iPad 3. Generation(2012)"
+    option14.style.display="unset"
+    option14.innerHTML="iPad 4. Generation(2012)"
+    option15.style.display="unset"
+    option15.innerHTML="iPad 5. Generation(2017)"
+
+    option16.style.display="unset"
+    option16.innerHTML="iPad 6. Generation(2018)"
+
+    option17.style.display="unset"
+    option17.innerHTML="iPad 7. Generation(2019)"
+    option18.style.display="unset"
+    option18.innerHTML="iPad 6. Generation(2018)"
+    option19.style.display="unset"
+    option19.innerHTML="iPad Air 1. Generation"
+    option20.style.display="unset"
+    option20.innerHTML="iPad Air 2. Generation(2014)"
+    option21.style.display="unset"
+    option21.innerHTML="iPad Air 3. Generation(2019)"
+    option22.style.display="unset"
+    option22.innerHTML="iPad Air 4. Generation(2020)"
+    option23.style.display="unset"
+    option23.innerHTML="iPad Mini 1. Generation(2012)"
+    option24.style.display="unset"
+    option24.innerHTML="iPad Mini 2. Generation(2013)"
+    option25.style.display="unset"
+    option25.innerHTML="iPad Mini 3. Generation(2014)"
+    option26.style.display="unset"
+    option26.innerHTML="iPad Mini 4. Generation(2015)"
+    option28.style.display="unset"
+    option28.innerHTML="iPad Mini 5(2019)"
+    option29.style.display="unset"
+    option29.innerHTML="iPad Pro 10.5 (2017)"
+    option30.style.display="unset"
+    option30.innerHTML="iPad Pro 11 (2018)"
+    option31.style.display="unset"
+    option31.innerHTML="iPad Pro 11 (2020)"
+    option32.style.display="unset"
+    option32.innerHTML="iPad Pro 12.9 1.Generation (2015)"
+    option33.style.display="unset"
+    option33.innerHTML="iPad Pro 12.9 2.Generation (2017)"
+
+
+    option34.style.display="unset"
+    option34.innerHTML="iPad Pro 12.9 3.Generation (2018)"
+    option35.style.display="unset"
+    option35.innerHTML="iPad Pro 12.9 4.Generation (2020)"
+    option36.style.display="unset"
+    option36.innerHTML="iPhone"
+    option37.style.display="unset"
+    option37.innerHTML="iPhone 11"
+    option38.style.display="unset"
+    option38.innerHTML="iPhone 11 Pro"
+    option39.style.display="unset"
+    option39.innerHTML="iPhone Pro Max"
+    option40.style.display="unset"
+    option40.innerHTML="iPhone 12"
+    option41.style.display="unset"
+    option41.innerHTML="iPhone 12 Mini"
+    option42.style.display="unset"
+    option42.innerHTML="iPhone 12 Pro"
+    option43.style.display="unset"
+    option43.innerHTML="iPhone 12 Pro Max"
+    option44.style.display="unset"
+    option44.innerHTML="iPhone 4"
+    option45.style.display="unset"
+    option45.innerHTML="iPhone 4s"
+    option46.style.display="unset"
+    option46.innerHTML="iPhone 5"
+    option47.style.display="unset"
+    option47.innerHTML="iPhone 5c"
+    option48.style.display="unset"
+    option48.innerHTML="iPhone 5s"
+    option49.style.display="unset"
+    option49.innerHTML="iPhone 6"
+    option50.style.display="unset"
+    option50.innerHTML="iPhone 6 Plus"
+    option51.style.display="unset"
+    option51.innerHTML="iPhone 6S"
+    option52.style.display="unset"
+    option52.innerHTML="iPhone 7"
+    option53.style.display="unset"
+    option53.innerHTML="iPhone 7 Plus"
+    option54.style.display="unset"
+    option54.innerHTML="iPhone 8"
+    option55.style.display="unset"
+    option55.innerHTML="iPhone 8 Plus"
+    option56.style.display="unset"
+    option56.innerHTML="iPhone X"
+    option57.style.display="unset"
+    option57.innerHTML="iPhone XR"
+    option58.style.display="unset"
+    option58.innerHTML="iPhone XS"
+    option59.style.display="unset"
+    option59.innerHTML="iPhone XS Max"
+  }
+  else if (current=="Samsung"){
+
+    auswahl.innerHTML="Samsung Geräte auswählen";
+
+
+  }
+  else if (current=="Google"){
+
+    auswahl.innerHTML="Google Geräte auswählen";
+
+
+  }
+  else if (current=="Sony"){
+
+    auswahl.innerHTML="Sony Geräte auswählen";
+
+
+  }
+  else if (current=="Huawei"){
+
+    auswahl.innerHTML="Huawei Geräte auswählen";
+
+
+  }
+  else if (current=="Motorola"){
+
+    auswahl.innerHTML="Motorola Geräte auswählen";
+
+
+  }
+  else if (current=="Oppo Reparatur"){
+
+    auswahl.innerHTML="Oppo Geräte auswählen";
+
+
+  }
+  else if (current=="Microsoft"){
+
+    auswahl.innerHTML="Microsoft Geräte auswählen";
+
+
+  }
+
+  else if (current=="LG"){
+
+    auswahl.innerHTML="LG Geräte auswählen";
+
+
+  }
+  else if (current=="Xiaomi"){
+
+    auswahl.innerHTML="Xiaomi Geräte auswählen";
+
+
+  }
+
+  else if (current=="OnePlus"){
+
+    auswahl.innerHTML="OnePlus Geräte auswählen";
+
+
+  }
+  else if (current=="Nokia Lumia"){
+
+    auswahl.innerHTML="Nokia Geräte auswählen";
+
+
+  }
+}
+
+
+
+function testul2(){
+
+    let select=document.getElementById("gerät")
+    let current2=select.options[select.selectedIndex].text;
+
+
+
+if (current2.includes("Series")){
+
+    optiona.style.display="unset"
+    optiona.innerHTML="Kostenvorschlag"
+    optionb.style.display="unset"
+    optionb.innerHTML="Glas Austausch"
+    optionc.style.display="unset"
+    optionc.innerHTML="Akku Austausch"
+    optiond.style.display="unset"
+    optiond.innerHTML="Display Reparatur"
+    optione.style.display="none"
+  optione.innerHTML="Series 3 Akku Austausch"
+  optionf.style.display="none"
+  optionf.innerHTML="Series 7 Display Reparatur"
+  optiong.style.display="none"
+  optiong.innerHTML="Series 7 Glas Austausch"
+  optionh.style.display="none"
+  optionh.innerHTML="Series SE Glas Austausch mit Saphirglas"
+  optioni.style.display="none"
+  optioni.innerHTML="Series 6 Glas Austausch mit Saphirglas"
+  optionj.style.display="none"
+  optionj.innerHTML="Series 5 Glas Austausch mit Saphirglas"
+  optionk.style.display="none"
+  optionk.innerHTML="Series 4 Glas Austausch mit Saphirglas"
+  optionl.style.display="none"
+  optionl.innerHTML="Series 1 Glas Austausch"
+  optionm.style.display="none"
+  optionm.innerHTML="Series 6 Glas Reparatur"
+  optionn.style.display="none"
+  optionn.innerHTML="SE Glas Reparatur"
+
+
+}
+ else if (current2.includes("Apple Watch")){
+
+  optiona.style.display="unset"
+  optiona.innerHTML="Kostenvorschlag"
+  optionb.style.display="unset"
+  optionb.innerHTML="Series 6 Akku Austausch"
+  optionc.style.display="unset"
+  optionc.innerHTML="Series 5 Akku Austausch"
+  optiond.style.display="unset"
+  optiond.innerHTML="Series 4 Akku Austausch"
+  optione.style.display="unset"
+  optione.innerHTML="Series 3 Akku Austausch"
+  optionf.style.display="unset"
+  optionf.innerHTML="Series 7 Display Reparatur"
+  optiong.style.display="unset"
+  optiong.innerHTML="Series 7 Glas Austausch"
+  optionh.style.display="unset"
+  optionh.innerHTML="Series SE Glas Austausch mit Saphirglas"
+  optioni.style.display="unset"
+  optioni.innerHTML="Series 6 Glas Austausch mit Saphirglas"
+  optionj.style.display="unset"
+  optionj.innerHTML="Series 5 Glas Austausch mit Saphirglas"
+  optionk.style.display="unset"
+  optionk.innerHTML="Series 4 Glas Austausch mit Saphirglas"
+  optionl.style.display="unset"
+  optionl.innerHTML="Series 1 Glas Austausch"
+  optionm.style.display="unset"
+  optionm.innerHTML="Series 6 Glas Reparatur"
+  optionn.style.display="unset"
+  optionn.innerHTML="SE Glas Reparatur"
+
+}
+else if (current2.includes("iPhone")){
+
+
+    optiona.style.display="unset"
+    optiona.innerHTML="Kostenvorschlag"
+    optionb.style.display="unset"
+    optionb.innerHTML="Datenrettung"
+    optionc.style.display="unset"
+    optionc.innerHTML="Rahmen Austausch"
+    optiond.style.display="unset"
+    optiond.innerHTML="Frontkamera Austausch"
+    optione.style.display="unset"
+  optione.innerHTML="Lautsprecher Austausch"
+  optionf.style.display="unset"
+  optionf.innerHTML="Ladebuchse Austausch"
+  optiong.style.display="unset"
+  optiong.innerHTML="Rückkamera Austausch"
+  optionh.style.display="unset"
+  optionh.innerHTML="Rückkameralinse Austausch"
+  optioni.style.display="unset"
+  optioni.innerHTML="On/Off Taste Austausch"
+  optionj.style.display="unset"
+  optionj.innerHTML="Akku Austausch"
+  optionk.style.display="unset"
+  optionk.innerHTML="Lautstärketasten Austausch"
+  optionl.style.display="unset"
+  optionl.innerHTML="Ohrmuschel Austausch"
+  optionm.style.display="unset"
+  optionm.innerHTML="Wlan Antenne Austausch"
+  optionn.style.display="unset"
+  optionn.innerHTML="Vibrationsmotor"
+  optiono.style.display="unset"
+  optiono.innerHTML="Microfon Austausch"
+  optionp.style.display="unset"
+  optionp.innerHTML="Backcover Austausch"
+
+
+}
+else if (current2.includes("iPad")){
+
+
+    optiona.style.display="unset"
+    optiona.innerHTML="Kostenvorschlag"
+    optionb.style.display="unset"
+    optionb.innerHTML="Datenrettung"
+    optionc.style.display="unset"
+    optionc.innerHTML="Touchscreen Reparatur"
+    optiond.style.display="unset"
+    optiond.innerHTML="LCD Reparatur"
+    optione.style.display="none"
+  optione.innerHTML="Lautsprecher Austausch"
+  optionf.style.display="none"
+  optionf.innerHTML="Ladebuchse Austausch"
+  optiong.style.display="none"
+  optiong.innerHTML="Rückkamera Austausch"
+  optionh.style.display="none"
+  optionh.innerHTML="Rückkameralinse Austausch"
+  optioni.style.display="none"
+  optioni.innerHTML="On/Off Taste Austausch"
+  optionj.style.display="none"
+  optionj.innerHTML="Akku Austausch"
+  optionk.style.display="none"
+  optionk.innerHTML="Lautstärketasten Austausch"
+  optionl.style.display="none"
+  optionl.innerHTML="Ohrmuschel Austausch"
+  optionm.style.display="none"
+  optionm.innerHTML="Wlan Antenne Austausch"
+  optionn.style.display="none"
+  optionn.innerHTML="Vibrationsmotor"
+  optiono.style.display="none"
+  optiono.innerHTML="Microfon Austausch"
+  optionp.style.display="none"
+  optionp.innerHTML="Backcover Austausch"
+
+
+}
+
+
+
+  
+
+}
+function retur(data){
+
+
+   window.open(data)
+
+
+
+}
+function testul3(){
+
+    let select=document.getElementById("reparatur")
+    let current3=select.options[select.selectedIndex].text;
+
+
+
+    if (current3=="Datenrettung"){
+     
+        retur("https://www.google.com/search?q=mozilla+firefox&oq=mozila&aqs=chrome.1.69i57j0i10l9.2358j0j15&sourceid=chrome&ie=UTF-8")
+    
+    }
+    
+}
+
+
