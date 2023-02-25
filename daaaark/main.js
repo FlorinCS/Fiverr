78f0473dd823e00bda0292ea9cb45b3599b4153e
FlorinCS <81389602+FlorinCS@users.noreply.github.com>
2023-02-25 19:47:07 +0100

    Add files via upload

---
+++ daaaark/main.js
@@ -0,0 +1,86 @@
+const calendarButton = document.querySelector(".btn-start");
+const calendarContainer = document.querySelector(".container");
+let image=document.getElementById('modal');
+
+const calendarDays = 24;
+ let actualDay = new Date().getDate();
+ //Here you can choose the day for testing purposes
+ //actualDay=2;
+ function setCookie() {
+    var arrCalendar = ['foo'];
+    var json_str_calendar = JSON.stringify(arrCalendar);
+    document.cookie = json_str_calendar;
+}
+let imageControll=0;
+
+//If you delete the comment you can delete the Cookie
+if(document.cookie==''){
+ setCookie()
+}
+const openDoor = (path, event) => {
+    
+    event.target.parentNode.style.backgroundImage = `url(${path})`;
+}
+
+const createCalendar = () => {
+    for(let i = 0; i  < calendarDays; i++) {
+        
+        const calendarDoor = document.createElement("div");
+        const calendarDoorText = document.createElement("div");
+
+        calendarDoor.classList.add("image");
+        calendarDoor.style.gridArea = "door" + (i + 1);
+        calendarContainer.appendChild(calendarDoor);
+
+        calendarDoorText.classList.add("text");
+        calendarDoorText.innerHTML = i + 1;
+        calendarDoor.appendChild(calendarDoorText);
+
+        courseNumber = i + 1;
+        let coursePath = `./Bilder/bild${courseNumber}.jpeg`;
+        if(i<actualDay){
+            if(JSON.parse(document.cookie).includes(calendarDoorText.innerText)){
+                
+                calendarDoorText.parentElement.style.backgroundImage = `url(${coursePath})`;
+            }
+        calendarDoorText.addEventListener("click", openDoor.bind(null,  coursePath));
+        calendarDoorText.addEventListener("click", myFunction);
+        calendarDoorText.parentElement.addEventListener("click", ImageControll);
+        image.addEventListener("click", ImageControll2);
+        }
+        function ImageControll(){
+           
+         if(imageControll==0){
+            image.style.display="unset";
+            image.style.backgroundImage='url('+coursePath+')';
+            image.style.backgroundSize="contain";
+            image.style.backgroundRepeat="no-repeat";
+            image.style.backgroundPosition="center";
+            image.style.backgroundColor="rgba(1,1,1,0.5)";
+            image.style.cursor="pointer";
+            imageControll=1;
+         }
+
+
+        }
+        function ImageControll2(){
+            if(imageControll==1){
+               image.style.display="none";
+               image.style.cursor="none";
+               imageControll=0;
+            }
+           }
+function myFunction(){
+    if(calendarDoorText.innerText<=actualDay){
+    if(JSON.parse(document.cookie).includes(calendarDoorText.innerText)==false){
+    let calendarCookie=JSON.parse(document.cookie);
+ console.log(calendarCookie.push(calendarDoorText.innerText));
+ document.cookie=JSON.stringify(calendarCookie);
+    }
+}
+}
+    }
+}
+
+
+calendarButton.addEventListener("click", createCalendar);
